9921a011a1f6f291d17e2d07365118a82c5d0c00
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-11 22:49:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import streamlit as st
 import pickle as pkl
 from PIL import Image
 import numpy as np
+
+class_list = {'0': 'Setosa', '1': 'Versicolor', '2': 'Virginica'}
+with open("styles.css") as f:
+    custom_css = f.read()
+st.title('Iris classification based on sepal and petal size')
+st.markdown(f"<style>{custom_css}</style>", unsafe_allow_html=True)
+
+st.image("image.jpg", use_column_width=True)
