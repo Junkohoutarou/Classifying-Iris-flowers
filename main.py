aa947b21a8c4d2e5d21410cf496b707a777e74ac
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-11 22:51:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ st.markdown(f"<style>{custom_css}</style>", unsafe_allow_html=True)
 st.image("image.jpg", use_column_width=True)
 input = open('lrc_iris.pkl', 'rb')
 model = pkl.load(input)
+
+st.header('Choose the size of sepal and petal size')
+
+sepal_length = st.slider('Sepal length (cm)', 0.0, 20.0, 0.0, 0.1)
+sepal_width = st.slider('Sepal width (cm)', 0.0, 20.0, 0.0, 0.1)
+petal_length = st.slider('Petal length (cm)', 0.0, 20.0, 0.0, 0.1)
+petal_width = st.slider('Petal width (cm)', 0.0, 20.0, 0.0, 0.1)
